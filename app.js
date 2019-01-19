d94f207e0368be8188c4f651b480bc3b76db3ea5
Derek <derekluu90@gmail.com>
2019-01-18 23:30:04 -0500

    completed css and include app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// Yummly app id 68ac39de
+// Yummly auth key 3f06f41e76c83498e7053f890bef8d89
+
+const apiID = "68ac39de"
+const apiKey = "3f06f41e76c83498e7053f890bef8d89"
+
+// const $yummlyResults = $.ajax({
+// 						   url: 'http://api.yummly.com/v1/api/recipes?'+'&maxTotalTimeInSeconds='+1700,
+// 						   type: 'GET',
+// 						   data: {
+// 						     format: 'jsonp',
+// 						   	 _app_id: '68ac39de',
+// 						   	 _app_key: '3f06f41e76c83498e7053f890bef8d89',
+// 						   	 rating: 5,
+// 						   	 maxResult: 9,
+// 							 requirePictures: true,
+// 							 q: 'party'
+// 						   }
+// 					   })
+					
