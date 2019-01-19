c353b56628fcaa91d59cc509bde2d0a1c7eda7e1
Derek <derekluu90@gmail.com>
2019-01-19 14:23:15 -0500

    added ajax to app.js

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-// Yummly app id 68ac39de
-// Yummly auth key 3f06f41e76c83498e7053f890bef8d89
+const apiID = "c98291d0";
+const apiKey = "6bb8f3a6880182e070150cf8780b037e";
+var $matches; //this holds the array of matches after ajax method runs
+const queryStrings = {
 
-const apiID = "68ac39de"
-const apiKey = "3f06f41e76c83498e7053f890bef8d89"
+}
 
-// const $yummlyResults = $.ajax({
-// 						   url: 'http://api.yummly.com/v1/api/recipes?'+'&maxTotalTimeInSeconds='+1700,
-// 						   type: 'GET',
-// 						   data: {
-// 						     format: 'jsonp',
-// 						   	 _app_id: '68ac39de',
-// 						   	 _app_key: '3f06f41e76c83498e7053f890bef8d89',
-// 						   	 rating: 5,
-// 						   	 maxResult: 9,
-// 							 requirePictures: true,
-// 							 q: 'party'
-// 						   }
-// 					   })
+$()
+//make each parameter an object - add the corresponding object to the data object in ajax method (for in loop)
+
+$(document).ready(function (){
+
+	const $initialResults = $.ajax({
+		url: 'http://api.yummly.com/v1/api/recipes?',
+		method: 'GET',
+		data: {
+			format: 'json',
+			_app_id: apiID,
+			_app_key: apiKey,
+			'allowedCourse[]': 'course^course-Main Dishes',
+			maxTotalTimeInSeconds: 1800,
+			requirePictures: true,
+		}
+	}).then(function (data){
+		$matches = data.matches;
+		console.log($matches[0].totalTimeInSeconds/60, 'minutes');
+		console.log($matches)
+		// console.log(data);
+	}); //end of ajax
+
+});//end of document ready
+
+//basic requirments for each search
+	//max results 6 - is there a point in having this?
+
+
+//display results 
+	//name of recipe $matches[i].recipeName
+	//img $matches[i].smallImageUrls[0]
+	//cooktime $matches[i].totalTimeInSeconds
+	//ingredients $matches[i].ingredients (returns an array)
+	//link to actual recipe
+
+//get data
+	//use correct query string based on user input
+	//make the form required 
+
+
+//display data
+
 					
