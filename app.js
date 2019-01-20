ccbb9c5dc4086d6e5de1f8704b6e79f434fc82d2
Derek <derekluu90@gmail.com>
2019-01-19 23:37:33 -0500

    updated app.js

--- app.js
+++ app.js
@@ -5,63 +5,34 @@ const queryStrings = {
 
 }
 
-$()
+const $recipes = $.ajax({
+	url: 'http://api.yummly.com/v1/api/recipes?',
+	method: 'GET',
+	data: {
+		format: 'json',
+		_app_id: apiID,
+		_app_key: apiKey,
+		'allowedCourse[]': 'course^course-Main Dishes',
+		maxTotalTimeInSeconds: 1800,	
+		requirePictures: true,
+		'allowedIngredient[]': 'pork',
+		'excludedIngredient[]': 'cheese',
+		q: '1 serving'
+		}
+}).then(function (data){
+	$matches = data.matches;
+	const $recipeID = $matches[2].id;
+	console.log('Matches array', $matches); //returns an array
+	console.log('Data Objects', data);
+	console.log(`Link to the recipe: https://www.yummly.com/recipe/${$recipeID}`);
+});
 //make each parameter an object - add the corresponding object to the data object in ajax method (for in loop)
 
 $(document).ready(function (){
-
-	// const $initialResults = $.ajax({
-	// 	url: 'http://api.yummly.com/v1/api/recipes?',
-	// 	method: 'GET',
-	// 	data: {
-	// 		format: 'json',
-	// 		_app_id: apiID,
-	// 		_app_key: apiKey,
-	// 		'allowedCourse[]': 'course^course-Main Dishes',
-	// 		maxTotalTimeInSeconds: 1800,	
-	// 		requirePictures: true,
-	// 		}
-	// 	}).then(function (data){
-	// 		$matches = data.matches;
-	// 		console.log($matches);
-	// 	});
-
-	async function getRecipes () {
-		const $recipes = await $.ajax({
-		url: 'http://api.yummly.com/v1/api/recipes?',
-		method: 'GET',
-		data: {
-			format: 'json',
-			_app_id: apiID,
-			_app_key: apiKey,
-			'allowedCourse[]': 'course^course-Main Dishes',
-			maxTotalTimeInSeconds: 1800,	
-			requirePictures: true,
-			}
-		})//wait until recipe ID is retrieved from the first endpoint
-		$matches = $recipes.matches;
-		console.log($matches);
-
-		const $recipesID = $.ajax ({
-			url: 'http://api.yummly.com/v1/api/recipe/recipe-id?',
-			method: 'GET',
-			data: {
-				format: 'json',
-				_app_id: apiID,
-				_app_key: apiKey
-			}
-		})
-		console.log($recipesID);
-	};
-
-	getRecipes();
+	$recipes;
 
 
-//create async function to fetch recipe ID, use recipe ID to fetch the URL
-//after the above is completed, then we apply it to the DOM
-//call the async function and pass it a callback function that applies what we need to the DOM
-
-});//end of document ready
+});
 
 //basic requirments for each search
 	//max results 6 - is there a point in having this?
@@ -72,11 +43,22 @@ $(document).ready(function (){
 	//img $matches[i].smallImageUrls[0]
 	//cooktime $matches[i].totalTimeInSeconds
 	//ingredients $matches[i].ingredients (returns an array)
-	//link to actual recipe
+	//link to actual recipe `https://www.yummly.com/recipe/${matches[i].id}`
 
-//get data
-	//use correct query string based on user input
-	//make the form required 
+//user search parameters
+	//nutritional values
+		// healthy: {'nutrition.CHOCDF.max': 100, carbs (less carbs from fibers)
+		// 			'nutrition.ENERC_KCAL.min': 400, min calories
+		// 			'nutrition.ENERC_KCAL.max': 800, max calories
+		// 			'nutrition.FAT.max': 30, total fat
+		// 			'nutrition.NA.max': 800 
+			// 		 }
+	//include and exclude
+		//'excludedIngredient[]': 
+		//'includedIngredient[]':
+	//servings
+
+
 
 
 //display data
